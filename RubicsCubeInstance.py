3104e0099136ee8c154e8499261971b55c0e83e2
alexander <sssasha@gmail.com>
2023-05-19 13:39:45 +0300

    added check_neighbors test function (and fixed bug in move_R())

--- RubicsCubeInstance.py
+++ RubicsCubeInstance.py
@@ -27,6 +27,61 @@ class RubicsCubeInstance:
         self.faces['B'] = ['G0', 'G1', 'G2', 'G3', 'G4', 'G5', 'G6', 'G7', 'G8']
         self.faces['D'] = ['Y0', 'Y1', 'Y2', 'Y3', 'Y4', 'Y5', 'Y6', 'Y7', 'Y8']
 
+    def check_neighbours(self):
+        neighbors = []
+        neighbors.append(sorted([self.faces['U'][1], self.faces['B'][1]]))
+        neighbors.append(sorted([self.faces['U'][3], self.faces['L'][1]]))
+        neighbors.append(sorted([self.faces['U'][5], self.faces['R'][1]]))
+        neighbors.append(sorted([self.faces['U'][7], self.faces['F'][1]]))
+        neighbors.append(sorted([self.faces['L'][3], self.faces['B'][5]]))
+        neighbors.append(sorted([self.faces['F'][3], self.faces['L'][5]]))
+        neighbors.append(sorted([self.faces['R'][3], self.faces['F'][5]]))
+        neighbors.append(sorted([self.faces['B'][3], self.faces['R'][5]]))
+        neighbors.append(sorted([self.faces['D'][1], self.faces['F'][7]]))
+        neighbors.append(sorted([self.faces['D'][3], self.faces['L'][7]]))
+        neighbors.append(sorted([self.faces['D'][5], self.faces['R'][7]]))
+        neighbors.append(sorted([self.faces['D'][7], self.faces['B'][7]]))
+        neighbors.append(sorted([self.faces['U'][0], self.faces['L'][0], self.faces['B'][2]]))
+        neighbors.append(sorted([self.faces['U'][2], self.faces['R'][2], self.faces['B'][0]]))
+        neighbors.append(sorted([self.faces['U'][6], self.faces['L'][2], self.faces['F'][0]]))
+        neighbors.append(sorted([self.faces['U'][8], self.faces['F'][2], self.faces['R'][0]]))
+        neighbors.append(sorted([self.faces['D'][0], self.faces['L'][8], self.faces['F'][6]]))
+        neighbors.append(sorted([self.faces['D'][2], self.faces['F'][8], self.faces['R'][6]]))
+        neighbors.append(sorted([self.faces['D'][6], self.faces['L'][6], self.faces['B'][8]]))
+        neighbors.append(sorted([self.faces['D'][8], self.faces['R'][8], self.faces['B'][6]]))
+        neighbors = sorted(neighbors)
+
+
+        neighbors2 = []
+        neighbors2.append(sorted(['W1', 'G1']))
+        neighbors2.append(sorted(['W3', 'R1']))
+        neighbors2.append(sorted(['W5', 'O1']))
+        neighbors2.append(sorted(['W7', 'B1']))
+        neighbors2.append(sorted(['R3', 'G5']))
+        neighbors2.append(sorted(['B3', 'R5']))
+        neighbors2.append(sorted(['O3', 'B5']))
+        neighbors2.append(sorted(['G3', 'O5']))
+        neighbors2.append(sorted(['Y1', 'B7']))
+        neighbors2.append(sorted(['Y3', 'R7']))
+        neighbors2.append(sorted(['Y5', 'O7']))
+        neighbors2.append(sorted(['Y7', 'G7']))
+        neighbors2.append(sorted(['W0', 'R0', 'G2']))
+        neighbors2.append(sorted(['W2', 'O2', 'G0']))
+        neighbors2.append(sorted(['W6', 'R2', 'B0']))
+        neighbors2.append(sorted(['W8', 'B2', 'O0']))
+        neighbors2.append(sorted(['Y0', 'R8', 'B6']))
+        neighbors2.append(sorted(['Y2', 'B8', 'O6']))
+        neighbors2.append(sorted(['Y6', 'R6', 'G8']))
+        neighbors2.append(sorted(['Y8', 'O8', 'G6']))
+        neighbors2 = sorted(neighbors2)
+
+        print("check: ", neighbors==neighbors2)
+        if neighbors != neighbors2:
+            print(neighbors)
+            print(neighbors2)
+        return neighbors==neighbors2
+    
+    
     def internal_rotate_face(self, face):
         keep = self.faces[face][0]
         self.faces[face][0] = self.faces[face][2]
@@ -66,8 +121,8 @@ class RubicsCubeInstance:
 
     def move_R(self):
         self.internal_rotate_face('R')
-        keep = self.faces['U'][2:9:3]
-        self.faces['U'][2:9:3] = self.faces['B'][0:9:3]
+        keep = self.faces['U'][8::-3]
+        self.faces['U'][8::-3] = self.faces['B'][0:9:3]
         self.faces['B'][0:9:3] = self.faces['D'][8::-3]
         self.faces['D'][8::-3] = self.faces['F'][8::-3]
         self.faces['F'][8::-3] = keep
@@ -94,12 +149,18 @@ class RubicsCubeInstance:
 
 cube = RubicsCubeInstance()
 #cube.faces['U'][0] = 'GG'
+
+cube.check_neighbours()
 cube.print_cube()
-cube.move_R()
+cube.move_F()
+cube.check_neighbours()
 cube.print_cube()
-cube.move_R()
+cube.move_F()
+cube.check_neighbours()
 cube.print_cube()
-cube.move_R()
+cube.move_F()
+cube.check_neighbours()
 cube.print_cube()
-cube.move_R()
+cube.move_F()
+cube.check_neighbours()
 cube.print_cube()
